5773859bef943f37c2099f4ed046f8e611e3c4db
Bright <bright.sablah@outlook.com>
2024-05-10 00:54:44 +0000

    rough work

---
+++ 13-add_two_digit_number.py
@@ -0,0 +1,3 @@
+num = input("enter two digit number: ")
+sum_ = int(num[0]) + int(num[1])
+print("sum of digits {} + {} = {}".format(num[0], num[1], sum_))
---
+++ rough_work.py
@@ -0,0 +1,2 @@
+print(0o123)
+print(0X123)
