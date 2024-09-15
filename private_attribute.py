7a4fbdc08922b969df971dee485ebd14e96e964a
bright@LATITUDE <bright.sablah@outlook.com>
2024-09-15 19:26:30 +0100

    test cases changed

--- private_attribute.py
+++ private_attribute.py
@@ -52,17 +52,10 @@ class Square:
 
 
 
-my_square_1 = Square(3)
-my_square_1.my_print()
-
+Square(3).my_print()
 print("--")
 
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
+Square(3, (1, 1)).my_print()
 print("--")
 
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
+Square(3, (3, 0)).my_print()
