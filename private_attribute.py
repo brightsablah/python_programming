276d2a0ea86ba5b83612a4e66d2b54937e5fc391
bright@LATITUDE <bright.sablah@outlook.com>
2024-09-15 19:34:55 +0100

    tuple check

--- private_attribute.py
+++ private_attribute.py
@@ -26,7 +26,7 @@ class Square:
     
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) == 2:
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if  not all(isinstance(i, int) and i > 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -60,4 +60,4 @@ print("--")
 
 Square(3, (3, 0)).my_print()
 
-Square(0, (0,0)).my_print()
+Square(0, (0,-3)).my_print()
